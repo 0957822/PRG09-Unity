f8f09483f4f08212d70d4cb12c0d5ee4edd2fbb5
Indra Doerga <0957822@hr.nl>
2022-03-20 18:17:26 +0100

    UnityPRG09
    Af

---
+++ UnityPRG09 0957822/Assets/Scripts/Goal3.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal3 : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.name == "Enemy1")
+        {
+            Debug.Log("Hit the goal");
+            Scoreboard.totalScore += 2;
+        }
+        if(collision.transform.name == "Enemy2")
+        {
+            Debug.Log("Hit the goal");
+            Scoreboard.totalScore += 1;
+        }
+        if(collision.transform.name == "Enemy3")
+        {
+            Debug.Log("Hit the goal");
+            Scoreboard.totalScore += 2;
+        }
+    }
+}
